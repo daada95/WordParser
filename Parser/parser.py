7523413f469eb0d6428927b3bcca20b75207523a
xwojziarnik <woj.ziarnik@gmail.com>
2022-04-21 08:43:11 +0200

    Modified parser

--- Parser/parser.py
+++ Parser/parser.py
@@ -10,20 +10,22 @@ def create_flashcards(path):
     text = str()
     title = str()
     for paragraph in docs.paragraphs:
-        if paragraph.style.name == 'Title':
-            title = paragraph.text
-        elif paragraph.style.name == "Heading 1":
-            heading = paragraph.text
-        elif paragraph.style.name == "Normal":
-            text = paragraph.text
-    flashcards[title] = heading
-    flashcards[heading] = text
+        if len(docs.paragraphs) > 1:
+            if paragraph.style.name == 'Title':
+                title = paragraph.text
+            elif paragraph.style.name == "Heading 1":
+                heading = paragraph.text
+            elif paragraph.style.name == "Normal":
+                text = paragraph.text
+            flashcards[title] = heading
+            flashcards[heading] = text
     return flashcards
 
 def flashcard_to_json():
     with open('json_file.json','w+') as json_file:
         json.dump(flashcards,json_file)
 
+
 if __name__ == '__main__':
-    create_flashcards('/Users/wojciechziarnik/Desktop/Test_files/plik_testowy.docx')
+    create_flashcards('/Users/wojciechziarnik/Desktop/Test_files/mongodb_commands.docx')
     flashcard_to_json()
