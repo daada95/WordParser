39ef3c6e4301440f67d97fd5e3d82cd8c2e5ddd5
xwojziarnik <woj.ziarnik@gmail.com>
2022-05-12 10:11:10 +0200

    Tried to parse received .docx, got errors. Next try tomorrow.

--- flask_files/app.py
+++ flask_files/app.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
+import os
 
 
 app = Flask(__name__)
@@ -11,37 +12,40 @@ cluster = MongoClient(CONNECTION_STRING, tls=True, tlsAllowInvalidCertificates=T
 db = cluster["parser"]
 collection = db["flashcards"]
 
-
-
 @app.route('/hello')
 def hello():
     return "<h1>Hello in Word Parser app</h1>"
 
+
 @app.route('/about')
 def about():
     return render_template("/about.html")
+
 
 @app.route('/upload-document', methods=["GET", "POST"])
 def upload_document():
 
     if request.method == "POST":
         email = request.form["email"]
-        print(f"I got your email adress! Is it {email}?")
+        print(email)
+
         if request.files:
             document = request.files["document"]
             print(document)
-            return f"File {document} uploaded by {email}."
+
+        return "Good job!"
+
     else:
         return render_template("upload_document.html")
+
 
 @app.route('/mongodb-connection')
 def connect_flask_to_mongodb():
     try:
-        db.flashcards.insert_one({"Connected": "True", "name": "Wojtek"})
+        db.flashcards.insert_one({upload_document.email: upload_document.main_dict})
+        return "Connected to MongoDB - great job!"
     except:
         return "Had an issue with inserting this to db"
-    return "Connected to MongoDB - great job!"
-
 
 
 if __name__ == '__main__':
