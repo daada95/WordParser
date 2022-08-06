80dae090535bd9579327ee49c7479b382665b3f3
xwojziarnik <woj.ziarnik@gmail.com>
2022-08-06 12:07:01 +0200

    Feature: home_page

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,6 +1,13 @@
 from . import app
+from flask import render_template
 
 
 @app.route('/hello-world')
 def hello_world_page():
     return "Hello, World!"
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
--- tests/hello_world_page.py
+++
@@ -1,9 +0,0 @@
-from flask_app import app
-
-
-def test_homepage():
-    response = app.test_client().get('/hello-world')
-
-    assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'Hello, World!'
-    assert response.data.decode('utf-8') != 'Welcome in Word Parser app!'
