8c1b960b7ecd8bda52b23041aa4d5b341bb46005
xwojziarnik <woj.ziarnik@gmail.com>
2022-08-16 18:38:59 +0200

    Feature: listing flashcards. Next step: add button to display content of the flashcard.

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,5 +1,6 @@
 from . import app
 from flask import render_template
+from flask import request
 
 
 @app.route('/hello-world')
@@ -11,3 +12,30 @@ def hello_world_page():
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+
+@app.route('/flashcards', methods=["GET", "POST"])
+def flashcards():
+    flashcards_example = [
+        {
+            "id": 1,
+            "title": "test1",
+            "content": "testcontent1",
+        },
+        {
+            "id": 2,
+            "title": "test2",
+            "content": "testcontent2",
+        },
+        {
+            "id": 3,
+            "title": "test3",
+            "content": "testcontent3",
+        }
+    ]
+
+    # if request.form['submit_button'] == 'Do Something':
+    if request.method == "POST":
+        return "Do something"
+
+    return render_template('flashcards.html', flashcards_example=flashcards_example)
