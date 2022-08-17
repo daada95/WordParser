ae96ad149a57381011cb80a36074b839b8d9f085
xwojziarnik <woj.ziarnik@gmail.com>
2022-08-17 18:57:43 +0200

    Test passed. Next chapter tomorrow.

--- parser_app/tests.py
+++ parser_app/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .views import index
 
 # Create your tests here.
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_index_view(self):
+        request = self.factory.get('')
+        response = index(request)
+        self.assertEquals(response.status_code, 200)
