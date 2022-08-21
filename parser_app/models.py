034412e9daf78ac2e56b3fe7ec1de13d5ba2d86d
xwojziarnik <woj.ziarnik@gmail.com>
2022-08-21 11:07:04 +0200

    Created second model -> FlashcardCategory.

--- parser_app/models.py
+++ parser_app/models.py
@@ -3,9 +3,16 @@ from django.db import models
 # Create your models here.
 
 
+class FlashcardCategory(models.Model):
+    category = models.CharField(max_length=30, unique=True, help_text="Category of flashcards.")
+
+    def __str__(self):
+        return self.category
+
+
 class Flashcard(models.Model):
     id = models.IntegerField(primary_key=True)
-    category = models.CharField(max_length=30, help_text="Category of flashcards.")
+    category = models.ForeignKey(FlashcardCategory, on_delete=models.CASCADE)
     title = models.CharField(max_length=50, help_text="Title of flashcard.")
     content = models.TextField(unique=True, help_text="Content (reverse) of flashcard.")
 
