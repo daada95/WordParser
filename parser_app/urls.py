3b9470bc8d8e0972714c1f6179b41ac6161f86c8
xwojziarnik <woj.ziarnik@gmail.com>
2022-08-21 11:02:40 +0200

    Fearure: CRUD - done.

--- parser_app/urls.py
+++ parser_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import HomePageView, AboutView, CategoryView, CategoryDetailView, FlashcardCreateView
+from .views import HomePageView, AboutView, FlashcardsCategoryView, FlashcardsCategoryDetailView, \
+    FlashcardCreate, FlashcardUpdate, FlashcardDelete
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home_page"),
     path('about', AboutView.as_view(), name="about"),
-    path('category', CategoryView.as_view(), name="category"),
-    path('category/<int:pk>', CategoryDetailView.as_view(), name="category_detail"),
-    path('create-flashcards/', FlashcardCreateView.as_view(), name="flashcard_form_view"),
+    path('flashcards', FlashcardsCategoryView.as_view(), name="category"),
+    path('flashcards/<int:pk>', FlashcardsCategoryDetailView.as_view(), name="category_detail"),
+    path('flashcards/add', FlashcardCreate.as_view(), name="flashcard_create"),
+    path('flashcards/<int:pk>/update', FlashcardUpdate.as_view(), name="flashcard_update"),
+    path('flashcards/<int:pk>/delete', FlashcardDelete.as_view(), name="flashcard_delete"),
 ]
--- parser_app/views.py
+++ parser_app/views.py
@@ -1,7 +1,7 @@
-from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Flashcard
-from .forms import FlashcardForm
 from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -14,12 +14,12 @@ class AboutView(TemplateView):
     template_name = 'about.html'
 
 
-class CategoryView(ListView):
+class FlashcardsCategoryView(ListView):
     template_name = 'category_list.html'
     model = Flashcard
 
 
-class CategoryDetailView(DetailView):
+class FlashcardsCategoryDetailView(DetailView):
     model = Flashcard
     template_name = "category_details.html"
 
@@ -27,10 +27,21 @@ class CategoryDetailView(DetailView):
         return get_object_or_404(Flashcard, id=self.kwargs.get("pk"))
 
 
-class FlashcardCreateView(FormView):
-    template_name = 'create_flashcard.html'
-    form_class = FlashcardForm
-    success_url = '/'
+class FlashcardCreate(CreateView):
+    template_name = "create_flashcard.html"
+    model = Flashcard
+    fields = ["category", "title", "content"]
+    success_url = reverse_lazy("home_page")
 
-    def form_valid(self, form):
-        return super().form_valid(form)
+
+class FlashcardUpdate(UpdateView):
+    template_name = "update_flashcard.html"
+    model = Flashcard
+    fields = ["category", "title", "content"]
+    success_url = reverse_lazy("category")
+
+
+class FlashcardDelete(DeleteView):
+    template_name = "delete_flashcard.html"
+    model = Flashcard
+    success_url = reverse_lazy("home_page")
