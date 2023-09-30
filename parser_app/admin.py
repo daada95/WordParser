39cc96d7e62734b77cf81b81a6c0b3162bfbf0e3
Wojciech Ziarnik <woj.ziarnik@gmail.com>
2023-09-30 16:24:20 +0200

    chore: cleanup for fastapi version of the project

--- parser_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Flashcard, FlashcardCategory
-
-# Register your models here.
-
-admin.site.register(Flashcard)
-admin.site.register(FlashcardCategory)
