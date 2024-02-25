3fa017d35c2220b4ed344df4d78a1dfb3db54c2d
Wojciech Ziarnik <woj.ziarnik@gmail.com>
2024-02-25 18:01:50 +0100

    chore: cleanup of old files

--- apps/errors.py
+++
@@ -1,8 +0,0 @@
-from typing import Union
-
-from pydantic import BaseModel
-
-
-class Error(BaseModel):
-    error: str
-    details: Union[str, dict, list]
--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from fastapi import FastAPI
 
 
 def setup_routing(app: FastAPI) -> None:
-    from apps.api import router as main_router
-    from apps.wordparser.parser_api import router as wordparser_router
-
-    app.include_router(main_router)
-    app.include_router(wordparser_router)
+    return
 
 
 def create_app() -> FastAPI:
