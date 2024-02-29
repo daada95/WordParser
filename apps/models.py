9d2d7d7f703694c6b47b110196b6b1e1ec6ead92
Wojciech Ziarnik <woj.ziarnik@gmail.com>
2024-02-29 20:20:55 +0100

    feat: CategoriesEnum class implemented

---
+++ apps/models.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class CategoriesEnum(Enum):
+    IDIOMS: str = "idioms"
+    TRANSPORT: str = "transport"
+    SCHOOL: str = "school"
+    PROFESSIONS: str = "professions"
+    IT: str = "it"
