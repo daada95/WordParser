f848037835e9d64504accc38ae8ec6c0a463df51
Wojciech Ziarnik <woj.ziarnik@gmail.com>
2024-03-06 18:15:30 +0100

    feat: implemented UnvalidatedWord

--- apps/models.py
+++ apps/models.py
@@ -1,4 +1,7 @@
+import uuid
 from enum import Enum
+
+from pydantic import BaseModel, Field
 
 
 class CategoriesEnum(Enum):
@@ -14,3 +17,20 @@ class EnglishLevelEnum(Enum):
     B1: str = "B1"
     B2: str = "B2"
     C1: str = "C1"
+
+
+class UnvalidatedWord(BaseModel):
+    _id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    word: str
+    category: str
+    level: str
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "word": "Break the ice",
+                "category": "idioms",
+                "level": "A1",
+            }
+        }
